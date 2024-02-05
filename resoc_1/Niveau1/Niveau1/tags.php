894a917d6928b69e475a0ed6754b3b87765e943e
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2024-02-05 14:11:58 +0100

    refactor V1

--- resoc_1/Niveau1/Niveau1/tags.php
+++ resoc_1/Niveau1/Niveau1/tags.php
@@ -1,75 +1,63 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Les message par mot-clé</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
 
-            </nav>
-        </header>
-        <div id="wrapper">
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Les message par mot-clé</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <div id="wrapper">
+        <?php
+        /**
+         * Cette page est similaire à wall.php ou feed.php 
+         * mais elle porte sur les mots-clés (tags)
+         */
+        /**
+         * Etape 1: Le mur concerne un mot-clé en particulier
+         */
+        $tagId = intval($_GET['tag_id']);
+        ?>
+        <?php
+        /**
+         * Etape 2: se connecter à la base de donnée
+         */
+        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+        ?>
+
+        <aside>
             <?php
             /**
-             * Cette page est similaire à wall.php ou feed.php 
-             * mais elle porte sur les mots-clés (tags)
+             * Etape 3: récupérer le nom du mot-clé
              */
-            /**
-             * Etape 1: Le mur concerne un mot-clé en particulier
-             */
-            $tagId = intval($_GET['tag_id']);
+            $laQuestionEnSql = "SELECT * FROM tags WHERE id= '$tagId' ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            $tag = $lesInformations->fetch_assoc();
+            //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
+            echo "<pre>" . print_r($tag, 1) . "</pre>";
             ?>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez les derniers messages comportant
+                    le mot-clé XXX
+                    (n°
+                    <?php echo $tagId ?>)
+                </p>
+
+            </section>
+        </aside>
+        <main>
             <?php
             /**
-             * Etape 2: se connecter à la base de donnée
+             * Etape 3: récupérer tous les messages avec un mot clé donné
              */
-            $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-            ?>
-
-            <aside>
-                <?php
-                /**
-                 * Etape 3: récupérer le nom du mot-clé
-                 */
-                $laQuestionEnSql = "SELECT * FROM tags WHERE id= '$tagId' ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $tag = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($tag, 1) . "</pre>";
-                ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé XXX
-                        (n° <?php echo $tagId ?>)
-                    </p>
-
-                </section>
-            </aside>
-            <main>
-                <?php
-                /**
-                 * Etape 3: récupérer tous les messages avec un mot clé donné
-                 */
-                $laQuestionEnSql = "
+            $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
                     users.alias as author_name,  
@@ -85,42 +73,41 @@
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if (!$lesInformations) {
+                echo ("Échec de la requete : " . $mysqli->error);
+            }
 
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 */
-                while ($post = $lesInformations->fetch_assoc())
-                {
+            /**
+             * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+             */
+            while ($post = $lesInformations->fetch_assoc()) {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
-                    ?>                
-                    <article>
-                        <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
-                        </h3>
-                        <address>par AreTirer</address>
-                        <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
-                        </div>                                            
-                        <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
-                        </footer>
-                    </article>
-                <?php } ?>
+                echo "<pre>" . print_r($post, 1) . "</pre>";
+                ?>
+                <article>
+                    <h3>
+                        <time datetime='2020-02-01 11:12:13'>31 février 2010 à 11h12</time>
+                    </h3>
+                    <address>par AreTirer</address>
+                    <div>
+                        <p>Ceci est un paragraphe</p>
+                        <p>Ceci est un autre paragraphe</p>
+                        <p>... de toutes manières il faut supprimer cet
+                            article et le remplacer par des informations en
+                            provenance de la base de donnée</p>
+                    </div>
+                    <footer>
+                        <small>♥ 132</small>
+                        <a href="">#lorem</a>,
+                        <a href="">#piscitur</a>,
+                    </footer>
+                </article>
+            <?php } ?>
 
 
-            </main>
-        </div>
-    </body>
+        </main>
+    </div>
+</body>
+
 </html>
