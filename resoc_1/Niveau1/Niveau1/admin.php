4569b6a6aaa883ba67800955fd6bd3bc88fbe725
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2024-02-05 15:51:31 +0100

    debug admin

--- resoc_1/Niveau1/Niveau1/admin.php
+++ resoc_1/Niveau1/Niveau1/admin.php
@@ -44,11 +44,15 @@
              * Attention à en pas oublier de modifier tag_id=321 avec l'id du mot dans le lien
              */
             while ($tag = $lesInformations->fetch_assoc()) {
-                echo "<pre>" . print_r($tag, 1) . "</pre>";
+                //echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <article>
-                    <h3>#chaussette</h3>
-                    <p>id:321</p>
+                    <h3>#
+                        <?php echo $tag['label'] ?>
+                    </h3>
+                    <p>
+                        <?php echo $tag['id'] ?>
+                    </p>
                     <nav>
                         <a href="tags.php?tag_id=321">Messages</a>
                     </nav>
@@ -75,11 +79,15 @@
              * Attention à en pas oublier de modifier dans le lien les "user_id=123" avec l'id de l'utilisatrice
              */
             while ($tag = $lesInformations->fetch_assoc()) {
-                echo "<pre>" . print_r($tag, 1) . "</pre>";
+                //echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <article>
-                    <h3>Alexandra</h3>
-                    <p>id:123</p>
+                    <h3>
+                        <?php echo $tag['alias'] ?>
+                    </h3>
+                    <p>
+                        <?php echo $tag['id'] ?>
+                    </p>
                     <nav>
                         <a href="wall.php?user_id=123">Mur</a>
                         | <a href="feed.php?user_id=123">Flux</a>
