1afe3ccf99d7692f85042aa5ec0eb36c672101fa
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2024-02-05 16:53:12 +0100

    debug tags, feed, wall

--- resoc_1/Niveau1/Niveau1/feed.php
+++ resoc_1/Niveau1/Niveau1/feed.php
@@ -40,13 +40,14 @@
             $lesInformations = $mysqli->query($laQuestionEnSql);
             $user = $lesInformations->fetch_assoc();
             //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-            echo "<pre>" . print_r($user, 1) . "</pre>";
+            //echo "<pre>" . print_r($user, 1) . "</pre>";
             ?>
             <img src="user.jpg" alt="Portrait de l'utilisatrice" />
             <section>
                 <h3>Présentation</h3>
                 <p>Sur cette page vous trouverez tous les message des utilisatrices
-                    auxquel est abonnée l'utilisatrice XXX
+                    auxquel est abonnée l'utilisatrice
+                    <?php echo $user['alias'] ?>
                     (n°
                     <?php echo $userId ?>)
                 </p>
@@ -79,30 +80,39 @@
                 echo ("Échec de la requete : " . $mysqli->error);
             }
 
-            /**
-             * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-             * A vous de retrouver comment faire la boucle while de parcours...
-             */
-            ?>
-            <article>
-                <h3>
-                    <time datetime='2020-02-01 11:12:13'>31 février 2010 à 11h12</time>
-                </h3>
-                <address>par AreTirer</address>
-                <div>
-                    <p>Ceci est un paragraphe</p>
-                    <p>Ceci est un autre paragraphe</p>
-                    <p>... de toutes manières il faut supprimer cet
-                        article et le remplacer par des informations en
-                        provenance de la base de donnée</p>
-                </div>
-                <footer>
-                    <small>♥ 132</small>
-                    <a href="">#lorem</a>,
-                    <a href="">#piscitur</a>,
-                </footer>
-            </article>
-            <?php
+            while ($post = $lesInformations->fetch_assoc()) {
+
+                /**
+                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+                 * A vous de retrouver comment faire la boucle while de parcours...
+                 */
+                ?>
+                <article>
+                    <h3>
+                        <time datetime='2020-02-01 11:12:13'>
+                            <?php echo $post['created'] ?>
+                        </time>
+                    </h3>
+                    <address>
+                        <?php echo $post['author_name'] ?>
+                    </address>
+                    <div>
+                        <?php echo $post['content'] ?>
+                    </div>
+                    <footer>
+                        <small>♥
+                            <?php echo $post['like_number'] ?>
+                        </small>
+                        <a href="">#
+                            <?php echo $post['taglist'] ?>
+                        </a>,
+
+                    </footer>
+                </article>
+
+                <?php
+            }
+            ;
             // et de pas oublier de fermer ici vote while
             ?>
 
