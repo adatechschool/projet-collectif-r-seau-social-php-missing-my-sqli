b340a9e269b5b5a73a541ba891e5254762c44347
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2024-02-05 11:20:37 +0100

    Niveau 1_init

---
+++ Niveau1/Niveau1/connect.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+
+
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Paramètres</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <header>
+        <img src="resoc.jpg" alt="Logo de notre réseau social" />
+        <nav id="menu">
+            <a href="news.php">Actualités</a>
+            <a href="wall.php?user_id=5">Mur</a>
+            <a href="feed.php?user_id=5">Flux</a>
+            <a href="tags.php?tag_id=1">Mots-clés</a>
+        </nav>
+        <nav id="user">
+            <a href="#">Profil</a>
+            <ul>
+                <li><a href="settings.php?user_id=5">Paramètres</a></li>
+                <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+            </ul>
+
+        </nav>
+    </header>
+</body>
+
+</html>
