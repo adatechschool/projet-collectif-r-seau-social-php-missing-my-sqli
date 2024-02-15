0f7e027ae54201f97bde8f962d4bcfe417b4b183
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-02-15 01:17:17 +0100

    news - minimum done

--- resoc_1/news.php
+++ resoc_1/news.php
@@ -12,169 +12,96 @@
     include 'header.php';
     ?>
 
-    <div id="wrapper">
-        <!-- <aside>
-            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
-            <section>
-                <h3>TEST</h3>
-                <p>Sur cette page vous trouverez les derniers messages de
-                    tous les utilisatrices du site.</p>
-            </section>
-        </aside> -->
-        <main>
-            <?php
+    <div class="center">
+        <?php
+        // Se connecte avec la base de données
+        include 'getDataBase.php';
+        // Vérifie
+        if ($mysqli->connect_errno) {
+            echo "<article>";
+            echo ("Échec de la connexion : " . $mysqli->connect_error);
+            echo ("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
+            echo "</article>";
+            exit();
+        }
 
-            // Se connecte avec la base de données
-            
-            include 'getDataBase.php';
-            // Vérifie
-            if ($mysqli->connect_errno) {
-                echo "<article>";
-                echo ("Échec de la connexion : " . $mysqli->connect_error);
-                echo ("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
-                echo "</article>";
-                exit();
-            }
+        // Interroge la base de données
+        $laQuestionEnSql = "
+                SELECT posts.content,
+                posts.created,
+                posts.user_id,
+                posts.id,
+                users.alias as author_name,  
+                count(likes.id) as like_number,  
+                GROUP_CONCAT(DISTINCT tags.id ORDER BY tags.label ASC) AS tag_ids,
+                GROUP_CONCAT(DISTINCT tags.label ORDER BY tags.label ASC) AS taglist 
+                FROM posts
+                JOIN users ON  users.id=posts.user_id
+                LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+                LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+                LEFT JOIN likes      ON likes.post_id  = posts.id 
+                GROUP BY posts.id
+                ORDER BY posts.created DESC  
+                LIMIT 20
+                ";
+        $lesInformations = $mysqli->query($laQuestionEnSql);
+        // Vérifie
+        if (!$lesInformations) {
+            echo "<article>";
+            echo ("Échec de la requete : " . $mysqli->error);
+            echo ("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
+            exit();
+        }
 
-            // Interroge la base de données
-            $laQuestionEnSql = "
-                    SELECT posts.content,
-                    posts.created,
-                    posts.user_id,
-                    posts.id,
-                    users.alias as author_name,  
-                    count(likes.id) as like_number,  
-                    GROUP_CONCAT(DISTINCT tags.id ORDER BY tags.label ASC) AS tag_ids,
-                    GROUP_CONCAT(DISTINCT tags.label ORDER BY tags.label ASC) AS taglist 
-                    FROM posts
-                    JOIN users ON  users.id=posts.user_id
-                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
-                    GROUP BY posts.id
-                    ORDER BY posts.created DESC  
-                    LIMIT 5
-                    ";
-            $lesInformations = $mysqli->query($laQuestionEnSql);
-            // Vérifie
-            if (!$lesInformations) {
-                echo "<article>";
-                echo ("Échec de la requete : " . $mysqli->error);
-                echo ("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
-                exit();
-            }
-
-            // Parcourt ces données et les range dans le html
-            
+        // Parcourt ces données et les range dans le html
 
 
 
-            while ($post = $lesInformations->fetch_assoc()) {
 
-                $postID = $post['id'];
+        while ($post = $lesInformations->fetch_assoc()) {
 
-                ?>
-                <!-- Affiche les articles -->
-                <div class="post">
-                    <div class="post-content">
-                        <div class="post-details">
-                            <a class="post-author" href="usersPosts.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name'] ?></a>
-                            <time class="post-date"><?php echo $post['created'] ?></time>
-                        </div>
-                        <p class="post-message"><?php echo $post['content'] ?></p>
-                    </div>
-                    <div class="post-bottom">
-                        <div class="post-tag">
-                        <p><?php
-                        $tags = explode(',', $post['taglist']); // Sépare les tags dans un tableau
-                        $tagIDs = explode(',', $post['tag_ids']);
-                        $totalTags = count($tags); // Récupère le nombre total de tags
-                        foreach ($tags as $index => $tag) {
-                            // Remets en forme chaque tag
-                            $tag = trim($tag);
-                            // Affiche chaque tag précédé d'un #
-                            echo '<a href="tags.php?tag_id=' . $tagIDs[$index] . '">#' . $tag . '</a>';
-                            // Ajoute une virgule si ce n'est pas le dernier tag
-                            if ($index < $totalTags - 1) {
-                                echo ', ';
-                            }
-                        }
-                        ?></p>
-                        </div>
-                        <div class="post-tag">
-                        ♥
-                            <?php echo $post['like_number'] ?>
-                            <!-- Ajoute le bouton like -->
-                            <?php include 'likeButton.php';
-                            ?>
-                    </div>
-                    </div>
-                </div>
-                <article>
-                    <h3>
-                        <time>
-                            <?php echo $post['created'] ?>
-                        </time>
-                    </h3>
-                    <address><a href="usersPosts.php?user_id=<?php echo $post['user_id'] ?>">
-                            <?php echo $post['author_name'] ?>
-                        </a>
-                    </address>
-                    <div>
-                        <p>
-                            <?php echo $post['content'] ?>
-                        </p>
-                    </div>
-                    <footer>
-                        <small>♥
-                            <?php echo $post['like_number'] ?>
-                            <!-- Ajoute le bouton like -->
-                            <?php include 'likeButton.php';
-                            ?>
-
-                        </small>
-                        <!-- Ajoute les tags -->
-                        <?php
-                        $tags = explode(',', $post['taglist']); // Sépare les tags dans un tableau
-                        $tagIDs = explode(',', $post['tag_ids']);
-                        $totalTags = count($tags); // Récupère le nombre total de tags
-                        foreach ($tags as $index => $tag) {
-                            // Remets en forme chaque tag
-                            $tag = trim($tag);
-                            // Affiche chaque tag précédé d'un #
-                            echo '<a href="tags.php?tag_id=' . $tagIDs[$index] . '">#' . $tag . '</a>';
-                            // Ajoute une virgule si ce n'est pas le dernier tag
-                            if ($index < $totalTags - 1) {
-                                echo ', ';
-                            }
-                        }
-                        ?>
-
-                    </footer>
-                </article>
-                <?php
-            }
+            $postID = $post['id'];
 
             ?>
-
-
-        </main>
-    </div>
-    <div class="center">
-        <div class="post">
-            <div class="post-content">
-                <div class="post-details">
-                    <a class="post-author" href="usersPosts.php?user_id=1">Pierre</a>
-                    <time class="post-date">2021-11-25 12:46:00</time>
+            <!-- Affiche les articles -->
+            <div class="post">
+                <div class="post-content">
+                    <div class="post-details">
+                        <a class="post-author" href="usersPosts.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name'] ?></a>
+                        <time class="post-date"><?php echo $post['created'] ?></time>
+                    </div>
+                    <p class="post-message"><?php echo $post['content'] ?></p>
                 </div>
-                <p class="post-message">Promesse d'embauche signée! Beaucoup de joie mais du stress en attendant le premier jour</p>
-            </div>
-            <div class="post-bottom">
-                <div class="post-tag">
-                    <p>test</p>
+                <div class="post-bottom">
+                    <div class="post-tag">
+                    <p><?php
+                    $tags = explode(',', $post['taglist']); // Sépare les tags dans un tableau
+                    $tagIDs = explode(',', $post['tag_ids']);
+                    $totalTags = count($tags); // Récupère le nombre total de tags
+                    foreach ($tags as $index => $tag) {
+                        // Remets en forme chaque tag
+                        $tag = trim($tag);
+                        // Affiche chaque tag précédé d'un #
+                        echo '<a href="tags.php?tag_id=' . $tagIDs[$index] . '">#' . $tag . '</a>';
+                        // Ajoute une virgule si ce n'est pas le dernier tag
+                        if ($index < $totalTags - 1) {
+                            echo ', ';
+                        }
+                    }
+                    ?></p>
+                    </div>
+                    <div class="post-tag">
+                    ♥
+                        <?php echo $post['like_number'] ?>
+                        <!-- Ajoute le bouton like -->
+                        <?php include 'likeButton.php';
+                        ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php
+        }
+        ?>
     </div>
 </body>
 
